3b40e4405904d74c73a556969441f02c4d332301
Pavel Shakhlovich <altmersc@gmail.com>
2025-03-25 23:25:11 +0100

    enable foreign key constraints

--- database/update_delete.py
+++ database/update_delete.py
@@ -3,6 +3,7 @@ import sqlite3
 
 with sqlite3.connect("university.db") as conn:
     cursor = conn.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
 
     cursor.execute("UPDATE Departments SET name = 'Chem' where name = 'Mech'")
     print(cursor.execute("SELECT * FROM Departments").fetchall())
@@ -10,5 +11,5 @@ with sqlite3.connect("university.db") as conn:
     cursor.execute("DELETE FROM Students WHERE roll = 15")
     print(cursor.execute("SELECT COUNT(*) FROM Students").fetchall()[0][0])
 
-    cursor.execute("DELETE FROM Departments WHERE depno = 40")  # should throw an error
+    cursor.execute("DELETE FROM Departments WHERE depno = 40")  # now this should throw an error if constraints exist
     print(cursor.execute("SELECT COUNT(*) FROM Departments").fetchall()[0][0])
